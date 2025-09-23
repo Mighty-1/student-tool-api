fdac78c51d42e3225055b16c96b56d4ef8cc0e07
Mighty Akuneme <Akunememighty721@gmail.com>
2025-09-23 04:45:04 +0100

    refactor: Update package.json and package-lock.json to include bcryptjs dependency; refactor authMiddleware to use ES6 module syntax

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const protect = require('../utils/authMiddleware');
+const express = require("express");
+const { protect } = require("../utils/authMiddleware");
 const router = express.Router();
 
-const stateRoutes = require('./state.routes');
-const instituteRoutes = require('./institute.routes');
-const instTypeRoutes = require('./instType.routes');
-const courseRoutes = require('./course.routes');
-const authRoutes = require('./auth.routes');
+const stateRoutes = require("./state.routes");
+const instituteRoutes = require("./institute.routes");
+const instTypeRoutes = require("./instType.routes");
+const courseRoutes = require("./course.routes");
+const authRoutes = require("./auth.routes");
 
 // Use the auth routes
-router.use('/auth', authRoutes);
+router.use("/auth", authRoutes);
 
 // Use the state routes
-router.use('/states', protect, stateRoutes);
+router.use("/states", protect, stateRoutes);
 // Use the institute routes
-router.use('/institutes', protect, instituteRoutes);
+router.use("/institutes", protect, instituteRoutes);
 // Use the institute type routes
-router.use('/institute-types', protect, instTypeRoutes);
+router.use("/institute-types", protect, instTypeRoutes);
 // Use the course routes
-router.use('/courses', protect, courseRoutes);
+router.use("/courses", protect, courseRoutes);
 
 // Export the router
 module.exports = router;
