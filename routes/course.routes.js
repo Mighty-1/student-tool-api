767bf9bbe23fa5d561f59f981234cc33680e2cad
Mighty Akuneme <Akunememighty721@gmail.com>
2025-09-24 14:05:07 +0100

    refactor: Add route to get courses by institute without parameters for improved query handling

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const courseController = require('../controllers/course.controller');
 
 router.get('/all-courses', courseController.getAllCourses);
+router.get('/get-courses-by-institute', courseController.getCourses); // for query param
 router.get('/get-courses-by-institute/:instituteId', courseController.getCourses);
 router.get('/subjects/:id', courseController.getSubjects);
 router.post('/add-course', courseController.createCourse);
